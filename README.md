d37403b6a11637aae5ae6bf5a7723f2e51feda0f
Yifei Xiong <yifei.xiong.1988@gmail.com>
2017-08-27 16:29:35 +0200

    add codes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RobotCar-Control-by-PWM
+
+This is part of my Bachelor Thesis.
---
+++ SpeedPWM.c
@@ -0,0 +1,66 @@
+void SpeedPWM(char PWMLine){
+	char PWMLine_L = PWMLine;
+	char PWMLine_R = PWMLine;
+	static char PWMCount_L = 0;
+	static char PWMCount_R = 0;
+	char Temp = 0;
+
+	if (FollowLineEnable == True){
+		Temp = (char)fabs((float)CG_X);
+		if (AdcValueFlag == 0){
+			Temp = 0;
+		}
+		else{
+			if (CG_X <0){
+				if ((Temp<<4) <= PWMLine_R){
+					PWMLine_R -= ((Temp<<5)+Temp<<2);
+				}
+				else{
+					PWMLine_R = 0;
+				}
+			}
+			else{
+				if ((Temp<<4) <= PWMLine_L){
+					PWMLine_L -= (Temp<<5);
+				}
+				else{
+					PWMLine_L = 0;
+				}
+			}
+		}
+	}
+
+	PWMCount_L ++;
+	PWMCount_R ++;
+
+	if (PWMCount_L > Fastest){
+		PWMCount_L = Stop;
+	}
+	if (PWMCount_R > Fastest){
+		PWMCount_R = Stop;
+	}
+	if (PWMCount_L < PWMLine_L){
+		switch (GoDirection){
+			case Front:Motor_Left_GoFront;break;
+			case Back:Motor_Left_GoBack;break;
+			case Left:Motor_Left_GoFront;break;
+			case Right:Motor_Left_GoBack;break;
+			case Stop:Motor_Left_Stop_Free;break;
+		}
+	}
+	else{
+		Motor_Left_Stop_Free;
+	}
+	if (PWMCount_R < PWMLine_R){
+		switch (GoDirection){
+			case Front:Motor_Right_GoFront;break;
+			case Back:Motor_Right_GoBack;break;
+			case Left:Motor_Right_GoBack;break;
+			case Right:Motor_Right_GoFront;break;
+			case Stop:Motor_Right_Stop_Free;break;
+		}
+	}
+	else{
+		Motor_Right_Stop_Free;
+	}
+}
